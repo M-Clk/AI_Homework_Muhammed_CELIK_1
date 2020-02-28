39b00dca7d625db580408019bc5f088d2a30381e
Muhammed Çelik <celikmuhammed21@hotmail.com>
2020-02-28 03:10:07 +0300

    Bitti

--- EigthPuzzleWithAStarAlgorithm/Form1.Designer.cs
+++ EigthPuzzleWithAStarAlgorithm/Form1.Designer.cs
@@ -30,7 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.label1 = new System.Windows.Forms.Label();
             this.btn0 = new System.Windows.Forms.Button();
             this.btn1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
@@ -42,24 +41,29 @@
             this.btn4 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.tmrAnimation = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 27);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(467, 252);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(607, 252);
             this.flowLayoutPanel1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(76, 382);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "goalState";
             // 
             // btn0
             // 
@@ -68,8 +72,10 @@
             this.btn0.Name = "btn0";
             this.btn0.Size = new System.Drawing.Size(35, 35);
             this.btn0.TabIndex = 3;
+            this.btn0.Tag = "0";
             this.btn0.Text = "1";
             this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn1
             // 
@@ -78,8 +84,10 @@
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(35, 35);
             this.btn1.TabIndex = 4;
+            this.btn1.Tag = "1";
             this.btn1.Text = "2";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn2
             // 
@@ -88,8 +96,10 @@
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(35, 35);
             this.btn2.TabIndex = 5;
+            this.btn2.Tag = "2";
             this.btn2.Text = "3";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn3
             // 
@@ -98,8 +108,10 @@
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(35, 35);
             this.btn3.TabIndex = 6;
+            this.btn3.Tag = "3";
             this.btn3.Text = "4";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn7
             // 
@@ -108,14 +120,16 @@
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(35, 35);
             this.btn7.TabIndex = 7;
+            this.btn7.Tag = "7";
             this.btn7.Text = "8";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btnCoz
             // 
-            this.btnCoz.Location = new System.Drawing.Point(427, 408);
+            this.btnCoz.Location = new System.Drawing.Point(251, 354);
             this.btnCoz.Name = "btnCoz";
-            this.btnCoz.Size = new System.Drawing.Size(117, 28);
+            this.btnCoz.Size = new System.Drawing.Size(117, 63);
             this.btnCoz.TabIndex = 9;
             this.btnCoz.Text = "Coz";
             this.btnCoz.UseVisualStyleBackColor = true;
@@ -128,18 +142,22 @@
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(35, 35);
             this.btn6.TabIndex = 10;
+            this.btn6.Tag = "6";
             this.btn6.Text = "7";
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn5
             // 
-            this.btn5.Location = new System.Drawing.Point(70, 35);
+            this.btn5.Location = new System.Drawing.Point(70, 70);
             this.btn5.Margin = new System.Windows.Forms.Padding(0);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(35, 35);
             this.btn5.TabIndex = 11;
+            this.btn5.Tag = "5";
             this.btn5.Text = "6";
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn4
             // 
@@ -148,8 +166,10 @@
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(35, 35);
             this.btn4.TabIndex = 12;
+            this.btn4.Tag = "4";
             this.btn4.Text = "5";
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn0_Click);
             // 
             // panel1
             // 
@@ -161,28 +181,172 @@
             this.panel1.Controls.Add(this.btn6);
             this.panel1.Controls.Add(this.btn3);
             this.panel1.Controls.Add(this.btn7);
-            this.panel1.Location = new System.Drawing.Point(424, 270);
+            this.panel1.Location = new System.Drawing.Point(12, 319);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(167, 132);
+            this.panel1.Size = new System.Drawing.Size(117, 109);
             this.panel1.TabIndex = 13;
             // 
             // tmrAnimation
             // 
+            this.tmrAnimation.Interval = 10;
             this.tmrAnimation.Tick += new System.EventHandler(this.tmrAnimation_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Adimlar";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button0);
+            this.panel2.Location = new System.Drawing.Point(511, 319);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(108, 109);
+            this.panel2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 35);
+            this.button1.TabIndex = 3;
+            this.button1.Tag = "0";
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 35);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(35, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Tag = "4";
+            this.button2.Text = "5";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(35, 0);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(35, 35);
+            this.button3.TabIndex = 4;
+            this.button3.Tag = "1";
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(70, 35);
+            this.button4.Margin = new System.Windows.Forms.Padding(0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(35, 35);
+            this.button4.TabIndex = 11;
+            this.button4.Tag = "5";
+            this.button4.Text = "6";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(70, 0);
+            this.button5.Margin = new System.Windows.Forms.Padding(0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(35, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Tag = "2";
+            this.button5.Text = "3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 70);
+            this.button6.Margin = new System.Windows.Forms.Padding(0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(35, 35);
+            this.button6.TabIndex = 10;
+            this.button6.Tag = "6";
+            this.button6.Text = "7";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 35);
+            this.button7.Margin = new System.Windows.Forms.Padding(0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(35, 35);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "3";
+            this.button7.Text = "4";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(35, 70);
+            this.button0.Margin = new System.Windows.Forms.Padding(0);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(35, 35);
+            this.button0.TabIndex = 7;
+            this.button0.Tag = "7";
+            this.button0.Text = "8";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(511, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Hedef Durum";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Baslangic Durumu";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(631, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnCoz);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -191,7 +355,6 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn0;
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btn2;
@@ -203,6 +366,18 @@
         private System.Windows.Forms.Button btn4;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Timer tmrAnimation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- EigthPuzzleWithAStarAlgorithm/Form1.cs
+++ EigthPuzzleWithAStarAlgorithm/Form1.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace EigthPuzzleWithAStarAlgorithm
@@ -15,41 +10,38 @@ namespace EigthPuzzleWithAStarAlgorithm
     {
         Button nextButton;
         List<int[]> states;
-        enum Move
-        {
-            Left,
-            Right,
-            Down,
-            Up
-        }
-        Move moveRotate;
+        int tickCount;
+        int statesIndex;
+        int plusX;
+        int plusY;
+        int[] goalState;
+        int[] initialState;
         public Form1() => InitializeComponent();
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            int[] goalState = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 0 };
-            int[] initialState = new int[] { 0, 3, 7, 1, 2, 8, 4, 5, 6 };
 
-            //TODO A* algoritmasina gore donguye koy. Acik ve kapali liste kavramlarini iyice anlamaya calis. 
-
-            Label lbl;
-            var stepManager = new StepManager(initialState, goalState);
-            states = stepManager.SolveProblem();
-
-            foreach (var item in states)
+        }
+        List<Button> SetIndexOfButtonsByLocation(Panel panel)
+        {
+            List<Button> buttons = new List<Button>();
+            foreach (Button item in panel.Controls)
             {
-                lbl = new Label() { AutoSize = true };
-                flowLayoutPanel1.Controls.Add(lbl);
-                ShowStep(item, lbl);
+                if (item.Location.Y == 0)
+                    item.Tag = item.Location.X / 35;
+                else if (item.Location.Y == 35)
+                    item.Tag = item.Location.X / 35 + 3;
+                else
+                    item.Tag = item.Location.X / 35 + 6;
+                buttons.Add(item);
             }
-            if (stepManager.IsSucceed) MessageBox.Show("Bulmaca cozuldu!!");
-            else MessageBox.Show("Yol bulunamadi.");
-
-            ShowStep(goalState, label1);
-
+            return buttons;
+        }
+        void SetLocationToInitialState()
+        {
             var firstState = states.FirstOrDefault();
             foreach (Button item in panel1.Controls)
-                item.Location = GetLocation(firstState.ToList().FindIndex(num => num == int.Parse(item.Text)));
+                item.Location = GetLocationByIndex(firstState.ToList().FindIndex(num => num == int.Parse(item.Text)));
         }
 
         void ShowStep(int[] state, Label label)
@@ -61,73 +53,127 @@ namespace EigthPuzzleWithAStarAlgorithm
                 else label.Text += state[i] + " ";
                 if ((i + 1) % 3 == 0) label.Text += "\n";
             }
+            label.Text += "______";
         }
 
         private void btnCoz_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i < states.Count(); i++)
+            goalState = new int[9];
+            initialState = new int[9];
+            flowLayoutPanel1.Controls.Clear();
+
+            SetIndexOfButtonsByLocation(panel1).OrderBy(btn =>
+            (int)btn.Tag).ToList().ForEach(btn =>
+            initialState[(int)btn.Tag] = int.Parse(btn.Text));
+
+            SetIndexOfButtonsByLocation(panel2).OrderBy(btn =>
+            (int)btn.Tag).ToList().ForEach(btn =>
+            goalState[(int)btn.Tag] = int.Parse(btn.Text));
+
+            Label lbl;
+            var stepManager = new StepManager(initialState, goalState);
+            states = stepManager.SolveProblem();
+
+            foreach (var item in states)
             {
-                var firstState = states[i - 1];
-                var currentState = states[i];
-                var blankIndex = firstState.ToList().FindIndex(num => num == 0);
-                int nextIndex = 0;
-                for (int j = 0; j < firstState.Length; j++)
-                    if (firstState[j] != currentState[j] && j != blankIndex) nextIndex = j;
-
-                nextButton = GetButtonByLocation(GetLocation(nextIndex));
-
-                if (nextIndex > blankIndex)
-                    if (nextIndex == blankIndex + 1)
-                        moveRotate = Move.Left;
-                    else moveRotate = Move.Up;
-                else
-                    if (nextIndex == blankIndex - 1)
-                    moveRotate = Move.Right;
-                else moveRotate = Move.Down;
-                tickCount = 35;
-                tmrAnimation.Start();
-                var a = 2;
+                lbl = new Label() { AutoSize = true };
+                flowLayoutPanel1.Controls.Add(lbl);
+                ShowStep(item, lbl);
             }
+            if (stepManager.IsSucceed)
+            {
+                MessageBox.Show("Bulmaca cozuldu! Adimlar teker teker uygulanacak.");
+                statesIndex = 1;
+                tmrAnimation.Start();
+            }
+            else
+            {
+                MessageBox.Show("Yol bulunamadi.");
+            }
+            btnCoz.Enabled = false;
 
         }
-        Button GetButtonByLocation(Point location)
+        Button GetButtonByLocation(Point location, Panel pnl)
         {
-            foreach (Button item in panel1.Controls)
+            foreach (Button item in pnl.Controls)
                 if (item.Location == location) return item;
-            return btn0;
+            return null;
         }
-        Point GetLocation(int index)
+        Point GetLocationByIndex(int index)
         {
             if (index < 3) return new Point(index * 35, 0);
             else if (index < 6) return new Point(index % 3 * 35, 35);
             else return new Point(index % 3 * 35, 70);
         }
-        int tickCount;
+
         private void tmrAnimation_Tick(object sender, EventArgs e)
         {
-            ChangeLocation();
-            if (tickCount == 0) tmrAnimation.Stop();
+            if (tickCount > 0)
+                ChangeLocationWithAutomation();
+            else if (statesIndex == states.Count())
+            {
+                tmrAnimation.Stop();
+                btnCoz.Enabled = true;
+            }
+            else ChangeNextState();
         }
-        void ChangeLocation()
+        void ChangeNextState()
+        {
+            var firstState = states[statesIndex - 1];
+            var currentState = states[statesIndex];
+            var blankIndex = firstState.ToList().FindIndex(num => num == 0);
+            int nextIndex = 0;
+            for (int j = 0; j < firstState.Length; j++)
+                if (firstState[j] != currentState[j] && j != blankIndex) nextIndex = j;
+
+            nextButton = GetButtonByLocation(GetLocationByIndex(nextIndex), (Panel)panel1);
+
+            if (nextIndex > blankIndex)
+                if (nextIndex == blankIndex + 1)
+                {
+                    plusX = -1;
+                    plusY = 0;
+                }
+                else
+                {
+                    plusX = 0;
+                    plusY = -1;
+                }
+
+            else
+                if (nextIndex == blankIndex - 1)
+            {
+                plusX = 1;
+                plusY = 0;
+            }
+            else
+            {
+                plusX = 0;
+                plusY = 1;
+            }
+            tickCount = 35;
+            statesIndex++;
+        }
+        void ChangeLocationWithAutomation()
         {
             tickCount--;
-            switch (moveRotate)
-            {
-                case Move.Left:
-                    nextButton.Location = new Point(nextButton.Location.X - 1, nextButton.Location.Y);
-                    break;
-                case Move.Right:
-                    nextButton.Location = new Point(nextButton.Location.X + 1, nextButton.Location.Y);
-                    break;
-                case Move.Down:
-                    nextButton.Location = new Point(nextButton.Location.X, nextButton.Location.Y + 1);
-                    break;
-                case Move.Up:
-                    nextButton.Location = new Point(nextButton.Location.X, nextButton.Location.Y - 1);
-                    break;
-                default:
-                    break;
-            }
+            nextButton.Location = new Point(nextButton.Location.X + plusX, nextButton.Location.Y + plusY);
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            if (!btnCoz.Enabled) return;
+            var btn = (Button)sender;
+            if (GetButtonByLocation(new Point(btn.Location.X + 35, btn.Location.Y), (Panel)btn.Parent) == null && btn.Location.X != 70)
+                btn.Location = new Point(btn.Location.X + 35, btn.Location.Y);
+            else if (GetButtonByLocation(new Point(btn.Location.X - 35, btn.Location.Y), (Panel)btn.Parent) == null && btn.Location.X != 0)
+                btn.Location = new Point(btn.Location.X - 35, btn.Location.Y);
+            else if (GetButtonByLocation(new Point(btn.Location.X, btn.Location.Y + 35), (Panel)btn.Parent) == null && btn.Location.Y != 70)
+                btn.Location = new Point(btn.Location.X, btn.Location.Y + 35);
+            else if (GetButtonByLocation(new Point(btn.Location.X, btn.Location.Y - 35), (Panel)btn.Parent) == null && btn.Location.Y != 0)
+                btn.Location = new Point(btn.Location.X, btn.Location.Y - 35);
+            btnCoz.Enabled = true;
+
         }
     }
 }
